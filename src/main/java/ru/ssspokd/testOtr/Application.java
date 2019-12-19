38b33dc61b0ba5454a3ed145e48985a08cd42337
e.nikolaev.stc <e.nikolaev.stc@innopolis.ru>
2019-12-19 21:23:52 +0300

    AAS

--- src/main/java/ru/ssspokd/testOtr/Application.java
+++ src/main/java/ru/ssspokd/testOtr/Application.java
@@ -1,9 +1,13 @@
+package ru.ssspokd.testOtr;
+
 import inet.ipaddr.IPAddress;
 import inet.ipaddr.IPAddressSeqRange;
 import inet.ipaddr.IPAddressString;
 import inet.ipaddr.ipv4.IPv4AddressSeqRange;
 import org.apache.log4j.Logger;
 import utils.Validate;
+
+import static java.lang.System.exit;
 
 
 public class Application {
@@ -14,6 +18,7 @@ public class Application {
         if(args.length != 2){
             LOGGER.error("need only 2 arguments \n" +
                     "example 192.168.0.1 192.168.0.100");
+            exit(-1);
         }
         Application application =  new Application();
         application.run(args[0],args[1]);
@@ -22,7 +27,7 @@ public class Application {
     public void run(String ip1, String ip2){
         if(!Validate.validateIP(ip1) || !Validate.validateIP(ip2)){
             LOGGER.error("ip1 or ip2 is not valid");
-            System.exit(-1);
+            exit(-1);
         }
         printBetweenIPRange(ip1,ip2);
     }
@@ -33,7 +38,8 @@ public class Application {
         IPAddress addr1 = ipAddressString1.getAddress(),
                 addr2 = ipAddressString2.getAddress();
         IPAddressSeqRange range = addr1.toSequentialRange(addr2);
-        System.out.println("count IP address" + ((IPv4AddressSeqRange) range).getIPv4Count());
+        long countIP = ((IPv4AddressSeqRange) range).getIPv4Count();
+        System.out.println("count IP address " + countIP);
         range.getIterable().forEach(LOGGER::info);
     }
 }
